4bc4d56b664b68df57497260f8fcc18ba5ef16b0
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-05-27 04:42:24 +0000

    Updates :/

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="description" content="Nothing but a Portfolio!" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400&display=swap"
           rel="stylesheet"
